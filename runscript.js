9a8af3d77a1a129f685989114aff26a143dcd8b1
krishan <kykrishanyadav@gmail.com>
2022-03-06 18:09:37 +0530

    initial-commit

---
+++ runscript.js
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(
+    callback: notify,
+  );
+Function notify(){
+    console.log("runnere");
+}
+// let vid = document.querySelectorAll(video)[0];
+// console.log(vid);
