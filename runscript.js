15a68492a9106f04fd147499d81d129f1b6e3b52
krishan <kykrishanyadav@gmail.com>
2022-03-12 15:02:31 +0530

    working-feature

--- runscript.js
+++ runscript.js
@@ -1,4 +1,4 @@
-// setInterval(skkip,1000);
+setInterval(skkip,1000);
 
 // let skip=()=>{
 //   if(document.querySelectorAll(".ytp-ad-module")[0].childNodes.length>0){
@@ -7,12 +7,14 @@
 // }
 // setTimeout(skkip,1000);
 
-// function skkip(){
+function skkip(){
 //   console.log("fnc called");
-//   if(document.querySelectorAll(".ytp-ad-module")[0].childNodes.length>0){
-//     console.log("ifcalled")
-//     document.querySelectorAll("video")[0].currentTime=90;
-//   }
-// }
+  if(document.querySelectorAll(".ytp-ad-text").length>0){
+    // console.log("ifcalled")
+    document.querySelectorAll("video")[0].currentTime=90;
+    let element=document.querySelector(".ytp-ad-skip-button");
+    element.click();
+  }
+}
 console.log("autocalled");
-
+window.addEventListener(onload,()=>console.log("onload"));
