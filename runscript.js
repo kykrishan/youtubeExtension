7d4b690fd326556b306582bdf65999d3b8076902
krishan yadav <kykrishanyadav@hotmail.com>
2022-12-17 15:38:25 +0530

    added forEach

--- runscript.js
+++ runscript.js
@@ -1,20 +1,22 @@
 setInterval(skkip,1000);
 
-// let skip=()=>{
-//   if(document.querySelectorAll(".ytp-ad-module")[0].childNodes.length>0){
-//     document.querySelectorAll("video")[0].currentTime=90;
-//   }
-// }
-// setTimeout(skkip,1000);
-
 function skkip(){
-//   console.log("fnc called");
+  console.log("fnc called");
+
   if(document.querySelectorAll(".ytp-ad-text").length>0){
-    // console.log("ifcalled")
+    console.log("Ads displayed...");
+    var videoDivNew = document.querySelectorAll("video");
+    videoDivNew.forEach(element => {
+      if(element.currentTime >0){
+        element.currentTime=100;
+      }
+    });
+
+
     document.querySelectorAll("video")[0].currentTime=90;
     let element=document.querySelector(".ytp-ad-skip-button");
     element.click();
   }
 }
 console.log("autocalled");
-window.addEventListener(onload,()=>console.log("onload"));
+// window.addEventListener(onload,()=>console.log("onload"));
